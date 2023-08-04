759fb4dbd0e7f231a57801834422b298900fb99f
itsobby <pascalobala9@gmail.com>
2023-08-04 12:15:43 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,56 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+* _pow - pow to 2
+* @num: number
+* @n: number of times
+* Return: result to pow to  n number
+*/
+unsigned int _pow(unsigned int num, unsigned int n)
+{
+	unsigned int sum, i;
+
+	sum = 1;
+	for (i = 0; i < n; i++)
+	{
+		sum = sum * num;
+
+	}
+	return (sum);
+}
+
+/**
+ * binary_to_uint - convert Binary to uint
+ * @b: binary number
+ * Return: the converted number or O
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
 
-	if (b == '\0')
+	unsigned int decimal, i, k;
+
+	decimal = 0;
+	i = 0;
+	decimal = 0;
+	if (b == NULL)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (k = 0; b[k] != '\0'; k++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (((b[k]) != '0') && ((b[k]) != '1') && ((b[k]) != '\0'))
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
 	}
-
-	return (num);
+	k = k - 1;
+	while (b[i] != '\0')
+	{
+		if ((b[k - i]) == '1')
+		{
+			if (i == 0)
+				decimal += 1;
+			else
+				decimal += _pow(2, i);
+		}
+		i++;
+	}
+	return (decimal);
 }
