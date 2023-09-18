339af154a879fd61d003a0d666e3d27a4dd02033
itsobby <pascalobala9@gmail.com>
2023-09-18 12:42:37 -0700

    Task 0

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - this funtion fills memory with a constant byte.
+ * @c: a constant byte.
+ * @n: number of bytes.
+ * @s: pointers to memory.
+ *
+ * Return: A pointer to memory area s.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
+
